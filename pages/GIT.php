6168e9c4c30d4b8288e8ac1a819b86b8ba0a2bc9
Unknown <annemijn.portier@hotmail.com>
2017-04-05 16:20:35 +0200

    added page Git with content

---
+++ pages/GIT.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="../css/stylesheet.css" media="screen" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link href='http://fonts.googleapis.com/css?family=Raleway:200,300' rel='stylesheet' type='text/css'>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+  <link rel="apple-touch-icon" sizes="57x57" href="../images/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="../images/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="../images/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="../images/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="../images/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="../images/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="../images/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="../images/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="../images/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="../images/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="../images/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../images/favicon-16x16.png">
+<link rel="manifest" href="../js/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="../images/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
+<title>A Creations</title>
+    <script src="http://d3js.org/d3.v3.min.js" language="JavaScript"></script>
+    <script src="../js/liquidFillGauge.js" language="JavaScript"></script>
+</head>
+
+<body id="project">
+
+<header>
+<div id="dl-menu" class="dl-menuwrapper">
+						<button class="dl-trigger">Open Menu</button>
+						<ul class="dl-menu">
+							<li>
+								<a href="about.php">About me</a>
+								
+									</li>
+									<li>
+										<a href="portfolio.php">Portfolio</a>
+                                        </li>
+							<li>
+								 <li><a href="drawings.php">Drawings</a></li>	
+                                  <li><a href="contact.php">Contact</a></li>	
+								
+							</li>
+									
+								
+						</ul>
+                        </div>
+<nav>
+<div class="wrapper">
+
+<div class="logo"><a href="../index.php" ><img src="../images/logo.png" alt="ACreations"></a></div>
+
+</div>
+</nav>
+</header>
+
+
+
+    <div class="project-title-git">
+            <h1>GIT Repository</h1>
+    </div>
+    <div class="wrapperproject">
+    <div style="display: none;" class="single-post-arrows single-prev-post"><span><a class="single-prev-post-a"></a></span></div>
+    <div class="single-post-arrows single-next-post"><span><a style="right: -546.2px;" href="hackathon.php" class="single-next-post-a"></a></span><a style="right: -246.2px; opacity: 0.2;" href="hackathon.php" rel="prev">Hackathon</a></div>
+    
+                     <div class="titelinleiding">
+            <span>
+            <h3>INLEIDING</h3>
+            </span>
+            </div>
+            
+    	<div class="projecttext">
+        <p>We kregen een workshop over GIT. Ik had hier vorig jaar al een beetje meegewerkt met semester 4 alleen had het toen niet vaak gebruikt. Nu wilde ik graag meer mee werken zodat ik het in de toekomst op projecten kan toepassen.</p>
+        </div>
+                 <div class="titeluitleg">
+            <span>
+            <h3>UITLEG</h3>
+            </span>
+            </div>
+            
+    	<div class="projecttext">
+        <p>We kregen eerst een tutorial om de basis van GIT te leren. Dit was voor mij erg fijn, omdat ik bijna alles was vergeten. Daarna zijn we Git en Github gaan installeren. Ik had al een account van Github door vorig jaar. Ik wist al een beetje hoe Github werkte, maar was veel vergeten. De docent raadde ons aan om onze portfolio op te zetten. Zo kan je meer met Git werken en krijg je een beter inzicht over Git. Ik had een repository voor mijn portfolio aangemaakt. Daarna heb ik de repository gecloned naar mijn laptop. Ik heb daarna bij een bestand wat code aangepast om te kijken of Git zag dat er wijzigingen waren gedaan. Ik zal Git blijven gebruiken als ik aan mijn portfolio werk. Hieronder is de link naar mijn repository zodat je ziet wat ik heb gedaan.</p>
+        <p><a href="https://github.com/annemijn/PortfolioAnnemijn">https://github.com/annemijn/PortfolioAnnemijn</a>
+
+        <p>Ik ben nu al een aantal dagen bezig met het maken van mijn portfolio door middel van GIT. Zoals je toenet las gebruikte ik hiervoor Github voor Windows. Na een aantal commits te hebben gemaakt, wilde ik hem refreshen. Het probleem hierbij was, was dat hij maar bleef refreshen zonder dat hij wijzigingen liet zien wat ik had gedaan. Ook update hij de repository op de website van Github niet meer. Ik ben toen maar een andere programma gaan gebruiken. Ik koos voor GitKraken. Ik zag dat andere klasgenoten hier ook mee werkte. Dit werkte prima. Ik kon alle wijzigingen van de branches zien en kon dit tegelijk ook op de website van Github zien. Verder kon ik branches aanmaken die meteen ook op de website van Github bij mijn repository gemaakt werden. Ik was blij dat het eindelijk werkte. Ik heb twee branches aangemaakt: een workshop-pages en homepage branch. Bij de workshop-pages zal ik wijzigingen maken bij de pagina’s van elke vak. Bij homepage zal ik wijzigingen maken bij de homepage zelf. Als ik iets af heb van een van de branches zal ik de branch mergen aan de master.</p>
+
+        <div id="gallery-3" class="gallery galleryid-2178 gallery-columns-3 gallery-size-thumbnail"><div class="clearboth"></div><dl class=" gallery-fullwidth">
+            <dt class="gallery-icon landscape">
+
+        <img src="../images/gitkraken.png"class="attachment-thumbnail" height="548" width="958">
+            </dt></dl>
+        </div>
+
+
+        <div class="titelreflectie">
+            <span>
+            <h3>REFLECTIE</h3>
+            </span>
+        </div>
+
+        <p>Ik vond het leerzaam om deze Git workshop te krijgen. De informatie die ik had over Git was minimaal. Nu dat ik de basis informatie over Git weet kan ik hier mee verder. Ik zal Git gaan gebruiken voor mijn portfolio. Aan het einde van het semester zal ik hier neerzetten wat voor ervaring ik ermee heb gehad.
+Met Git heb ik twee programma’s (Github en gitkraken) uitgeprobeerd om met Git te werken. Ik vond het erg leerzaam om te zien wat de verschillen waren tussen deze twee programma’s. Er zaten veel bugs in bij het programma Github. Ik merkte dat ik niet als enige was die deze bug had waardoor hij niet wilde refreshen. Veel mensen klaagde er op het internet over. Verder vond ik de interface van Github wel fijn werken. Daarna heb ik GitKraken uitgeprobeerd. Ik merkte dat ik wel even moest wennen aan de interface. Dit was voor mij een beetje onoverzichtelijk. Na een tutorial te hebben gekeken snapte ik het en was het heel makkelijk. Het was erg fijn dat ik alles kon updaten zodat het meteen met mijn Github repository werd geupdate. Verder heb ik branches aangemaakt en weet ik nu ook hoe ik daarmee moet werken. Ik vind het erg fijn dat ik me goed heb verdiept in Git. Ik weet nu hoe je Git moet toepassen op een project. Hiermee kan ik in de toekomst ook werken met Git bij groepsprojecten.
+</p><br>
+        </div>
+    </div>
+
+        <footer>
+                <div class="contacticon">
+        <div class="emailicondiv">
+        <a href="mailto:annemijn.portier@hotmail.com"><img id="emailicon" src="../images/emailicon.png"/></a>
+        </div>
+        <div class="linkedinicondiv">
+        <a href="https://www.linkedin.com/in/annemijn-portier-8b7914115?trk=nav_responsive_tab_profile"><img id="linkedinicon" src="../images/linkedinicon.png"/></a>
+      
+        </div>
+        </div>
+        <p>Copyright 2016 ACreations, All Rights Reserved.</p>
+        </footer>
+                        <script src="../js/modernizr.custom.js"></script>
+        		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script src="../js/jquery.dlmenu.js"></script>
+		<script>
+			$(function() {
+				$( '#dl-menu' ).dlmenu();
+			});
+		</script>
+<script src="../js/jquery.carouFredSel-6.0.2-packed.js" type="text/javascript"></script>
+<script src="../js/jquery.prettyPhoto.js" type="text/javascript"></script>
+<script src="../js/waypoints.min.js" type="text/javascript"></script>
+<script src="../js/custom.js" type="text/javascript"></script>
+
+</body>
+</html>
