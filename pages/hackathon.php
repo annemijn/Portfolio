a1b9daff14186560c38ff0c81d9571bfcffbc146
Unknown <annemijn.portier@hotmail.com>
2017-04-05 17:49:15 +0200

    added page Hacakthon with content

---
+++ pages/hackathon.php
@@ -0,0 +1,177 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="../css/stylesheet.css" media="screen" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link href='http://fonts.googleapis.com/css?family=Raleway:200,300' rel='stylesheet' type='text/css'>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+  <link rel="apple-touch-icon" sizes="57x57" href="../images/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="../images/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="../images/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="../images/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="../images/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="../images/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="../images/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="../images/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="../images/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="../images/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="../images/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../images/favicon-16x16.png">
+<link rel="manifest" href="../js/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="../images/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
+<title>A Creations</title>
+    <script src="http://d3js.org/d3.v3.min.js" language="JavaScript"></script>
+    <script src="../js/liquidFillGauge.js" language="JavaScript"></script>
+</head>
+
+<body id="project">
+
+<header>
+<div id="dl-menu" class="dl-menuwrapper">
+						<button class="dl-trigger">Open Menu</button>
+						<ul class="dl-menu">
+							<li>
+								<a href="about.php">About me</a>
+								
+									</li>
+									<li>
+										<a href="portfolio.php">Portfolio</a>
+                                        </li>
+							<li>
+								 <li><a href="drawings.php">Drawings</a></li>	
+                                  <li><a href="contact.php">Contact</a></li>	
+								
+							</li>
+									
+								
+						</ul>
+                        </div>
+<nav>
+<div class="wrapper">
+
+<div class="logo"><a href="../index.php" ><img src="../images/logo.png" alt="ACreations"></a></div>
+
+</div>
+</nav>
+</header>
+
+
+
+    <div class="project-title-maquette">
+            <h1>Hackathon</h1>
+    </div>
+    <div class="wrapperproject">
+    <div class="single-post-arrows single-prev-post"><span><a style="left: -565.2px;" href="GIT.php" class="single-prev-post-a"></a></span><a style="left: -565.2px; opacity: 0.2;" href="`GIT.php" rel="next">Git Repository</a></div>
+    <div class="single-post-arrows single-next-post"><span><a style="right: -546.2px;" href="media.php" class="single-next-post-a"></a></span><a style="right: -246.2px; opacity: 0.2;" href="media.php" rel="prev">media-en contenstrategie</a></div>
+    
+                     <div class="titelinleiding">
+            <span>
+            <h3>INLEIDING</h3>
+            </span>
+            </div>
+            
+    	<div class="projecttext">
+        <p>Fontys had een evenement georganiseerd genaamd Hackathon. Hierbij ga je 24 uur aan een opdracht werken. Wij hadden als opdracht om een datavisualisatie te maken voor de MirrorRoom van We Are Data.</p>
+        </div>
+                 <div class="titeluitleg">
+            <span>
+            <h3>UITLEG</h3>
+            </span>
+            </div>
+            
+    	<div class="projecttext">
+
+
+        <div id="gallery-3" class="gallery galleryid-2178 gallery-columns-3 gallery-size-thumbnail"><div class="clearboth"></div><dl class=" gallery-fullwidth">
+            <dt class="gallery-icon landscape">
+
+        <img src="../images/hackathon1.jpg"class="attachment-thumbnail" height="548" width="958">
+            </dt></dl>
+        </div>
+
+        <p>De Hackathon werd gehouden in de Effenaar in Eindhoven. Om 13.00 uur begon de kick-off. Daarbij kregen we onze opdracht te horen en in welke groepje we zaten. Omdat niet iedereen was opkomen dagen, ben ik bij een ander groepje gaan zitten. We zijn als eerste de data gaan uitlezen die we hadden meegekregen. We kregen een CSV en een JSON bestand. Uiteindelijk kwamen we erachter dat er in de JSON bestand een error zat. Hierdoor moesten we op wachten tot we een verbeterede versie kregen. Uiteindelijk kregen we die en gingen we brainstormen welke onderwerpen ons interessant leek om op te filteren. We vonden het interessant om ons te richten op het geslacht. Bijvoorbeeld hoeveel vrouwen er vergeleken met mannen de MirrorRoom bezochten. De onderwerpen die ons interessant leken waren het verschil tussen mannen en vrouwen of ze hun data gingen publiceren, wat hun grootste angst was, de locatie en gewicht.</p>
+
+        <div id="gallery-3" class="gallery galleryid-2178 gallery-columns-3 gallery-size-thumbnail"><div class="clearboth"></div><dl class=" gallery-fullwidth">
+            <dt class="gallery-icon landscape">
+
+        <img src="../images/hackathon2.jpg"class="attachment-thumbnail" height="548" width="958">
+            </dt></dl>
+        </div>
+
+
+        <p>Ik had al een keer een datavisualisatie gemaakt die de verschillen liet zien tussen mannen en vrouwen voor een schoolproject. Dit leek mij wel leuk om dit te gaan verbeteren en uit te werken op basis van deze data. Met behulp van Javascript roep ik de JSON bestand op. Hierin staan alle data van de MirrorRoom. Deze gegevens worden getoond door middel van een percentage die wordt berekend met JQuery. Daarna heb ik de visualisatie erbij gemaakt. De poppetjes had ik in het vorige project al uitgetekend. Nu heb ik er een mooie polygon afbeelding achter de poppetjes gezet. Deze stijl paste goed bij We Are Data. De polygon afbeelding heb ik daarna natuurlijk in het roze en blauw gemaakt waardoor je het verschil tussen man en vrouw goed zag. De afbeelding werd door middel van JQuery naar boven geanimeerd waardoor je een mooie animatie kreeg. Ik heb ook een menu er bij gemaakt waarmee je kon switchen naar een andere onderwerp. We hadden twee onderwerpen uitgewerkt. Dit was het verschil van de opkomst tussen mannen en vrouwen en het verschil wie er hun profiel had gepubliceerd tussen mannen en vrouwen. <a href="http://i315962.iris.fhict.nl/hackathon/dv.html">Hier</a> kan je naar de datavisualisatie gaan.</p>
+
+        <div id="gallery-3" class="gallery galleryid-2178 gallery-columns-3 gallery-size-thumbnail"><div class="clearboth"></div><dl class=" gallery-fullwidth">
+            <dt class="gallery-icon landscape">
+
+        <img src="../images/hackathon.jpg"class="attachment-thumbnail" height="548" width="958">
+            </dt></dl>
+        </div>
+
+        <p>Tussendoor ben ik nog in de MirrorRoom geweest. Dit was erg interessant. Ik wist al wel van tevoren wat er ging gebeuren, dus het was minder spannend. Verder vond ik het heel goed gemaakt en was het interessant om te zien wat voor data de MirrorRoom van jou kan zien. </p>
+
+        <div id="gallery-3" class="gallery galleryid-2178 gallery-columns-3 gallery-size-thumbnail"><div class="clearboth"></div><dl class=" gallery-fullwidth">
+            <dt class="gallery-icon landscape">
+
+        <img src="../images/hackathon5.jpg"class="attachment-thumbnail" height="548" width="958">
+            </dt></dl>
+        </div>
+
+        <p>Ik ben tot 20.00 uur gebleven om de datavisualisatie te maken. Van ons groepje waren er nog maar drie over. De andere twee bleven ook niet meer en heb ik gekozen om ook naar huis te gaan.</p>
+
+        <p>De volgende ochtend ben ik weer naar de Effenaar gegaan. We hadden nog aan de datavisualisatie gewerkt. De opkomst van de Hackathon was nog kleiner als gisteren. Daarom zijn we met een aantal groepjes bij elkaar gaan zitten en een groep gaan vormen. We hebben elkaars werk laten zien en gekeken wat er overeenkwam. Iedereen was er mee eens om meer te verdieppen tussen mannen en vrouwen. Iedereen maakte een datavisualisatie met een andere onderwerp van het verschil tussen mannen en vrouwen. Mijn datavisualisatie had ik ook nog een beetje aangepast. Iedereen vond de datavisualisatie visueel heel mooi uitzien.
+        <br>
+        Daarna kwamen de juryleden en gingen ze langs elk groepje. De jury was onder de indruk van het visueel aspect van mijn datavisualisatie. Als groepje moesten we daarna presenteren. Hierbij lieten wij al onze datavisualisaties zien. Jammer genoeg hadden we geen prijs gewonnen.</p>
+
+        <div id="gallery-3" class="gallery galleryid-2178 gallery-columns-3 gallery-size-thumbnail"><div class="clearboth"></div><dl class=" gallery-fullwidth">
+            <dt class="gallery-icon landscape">
+
+        <img src="../images/hackathon4.jpg"class="attachment-thumbnail" height="548" width="958">
+            </dt></dl>
+        </div>
+
+
+        <div class="titelreflectie">
+            <span>
+            <h3>REFLECTIE</h3>
+            </span>
+        </div>
+
+        <p>Ik vond het interessant om een keer een hackathon mee te maken. Ik vond het heel leuk om nieuwe mensen te leren kennen. Verder deden er ook internationale studenten mee. Aan de ene kant vond ik dit leuk om mee te maken, maar aan de andere kant was het heel lastig om met hun te communiceren. Per groep was de verhouding ook niet gelijk tussen internationale en Fontys studenten. Hierdoor ging je al gauw in het Nederlands praten. De MirrorRoom vond ik erg interessant om mee te maken. Zelf had ik al met de eyetracker voor de MirrorRoom in semester 3 moeten werken. Het was erg vet om dit in het echt te zien. Ook heb ik geleerd op welke manieren je een datavisualisatie kan maken. Ik vond het heel fijn dat iedereen enthousiast was over mijn datavisualisatie. Hier was ik trots op.
+</p><br>
+        </div>
+    </div>
+
+        <footer>
+                <div class="contacticon">
+        <div class="emailicondiv">
+        <a href="mailto:annemijn.portier@hotmail.com"><img id="emailicon" src="../images/emailicon.png"/></a>
+        </div>
+        <div class="linkedinicondiv">
+        <a href="https://www.linkedin.com/in/annemijn-portier-8b7914115?trk=nav_responsive_tab_profile"><img id="linkedinicon" src="../images/linkedinicon.png"/></a>
+      
+        </div>
+        </div>
+        <p>Copyright 2016 ACreations, All Rights Reserved.</p>
+        </footer>
+                        <script src="../js/modernizr.custom.js"></script>
+        		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script src="../js/jquery.dlmenu.js"></script>
+		<script>
+			$(function() {
+				$( '#dl-menu' ).dlmenu();
+			});
+		</script>
+<script src="../js/jquery.carouFredSel-6.0.2-packed.js" type="text/javascript"></script>
+<script src="../js/jquery.prettyPhoto.js" type="text/javascript"></script>
+<script src="../js/waypoints.min.js" type="text/javascript"></script>
+<script src="../js/custom.js" type="text/javascript"></script>
+
+</body>
+</html>
