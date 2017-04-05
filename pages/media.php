705ee4ebdbea47d5fddba49054b82373289c4555
Unknown <annemijn.portier@hotmail.com>
2017-04-05 18:02:34 +0200

    added page Media with half of the content

---
+++ pages/media.php
@@ -0,0 +1,150 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="../css/stylesheet.css" media="screen" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link href='http://fonts.googleapis.com/css?family=Raleway:200,300' rel='stylesheet' type='text/css'>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+  <link rel="apple-touch-icon" sizes="57x57" href="../images/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="../images/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="../images/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="../images/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="../images/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="../images/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="../images/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="../images/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="../images/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="../images/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="../images/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../images/favicon-16x16.png">
+<link rel="manifest" href="../js/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="../images/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
+<title>A Creations</title>
+    <script src="http://d3js.org/d3.v3.min.js" language="JavaScript"></script>
+    <script src="../js/liquidFillGauge.js" language="JavaScript"></script>
+</head>
+
+<body id="project">
+
+<header>
+<div id="dl-menu" class="dl-menuwrapper">
+						<button class="dl-trigger">Open Menu</button>
+						<ul class="dl-menu">
+							<li>
+								<a href="about.php">About me</a>
+								
+									</li>
+									<li>
+										<a href="portfolio.php">Portfolio</a>
+                                        </li>
+							<li>
+								 <li><a href="drawings.php">Drawings</a></li>	
+                                  <li><a href="contact.php">Contact</a></li>	
+								
+							</li>
+									
+								
+						</ul>
+                        </div>
+<nav>
+<div class="wrapper">
+
+<div class="logo"><a href="../index.php" ><img src="../images/logo.png" alt="ACreations"></a></div>
+
+</div>
+</nav>
+</header>
+
+
+
+    <div class="project-title-video">
+            <h1>Media- en contentstrategie</h1>
+    </div>
+    <div class="wrapperproject">
+    <div class="single-post-arrows single-prev-post"><span><a style="left: -565.2px;" href="sprint1.php" class="single-prev-post-a"></a></span><a style="left: -565.2px; opacity: 0.2;" href="sprint1.php" rel="next">Sco sprint 1</a></div>
+    <div class="single-post-arrows single-next-post"><span><a style="right: -546.2px;" href="media.php" class="single-next-post-a"></a></span><a style="right: -246.2px; opacity: 0.2;" href="media.php" rel="prev">media-en contenstrategie</a></div>
+    
+                     <div class="titelinleiding">
+            <span>
+            <h3>INLEIDING</h3>
+            </span>
+            </div>
+            
+    	<div class="projecttext">
+        <p>Voor ons concept gingen we een media-en contenstrategie maken. Hierbij maken we gebruik van de customer journey om het helderder te maken.</p>
+        </div>
+                 <div class="titeluitleg">
+            <span>
+            <h3>UITLEG</h3>
+            </span>
+            </div>
+            
+    	<div class="projecttext">
+        <p>Voor de mediastrategie hebben we het diagram van paid,owned,earned en social media bijgenomen. Bij elke zijn we media gaan verzinnen voor onze campagne. Deze zie je hieronder. </p>
+        
+
+        <div id="gallery-3" class="gallery galleryid-2178 gallery-columns-3 gallery-size-thumbnail"><div class="clearboth"></div><dl class=" gallery-fullwidth">
+            <dt class="gallery-icon landscape">
+
+        <img src="../images/media2.jpg"class="attachment-thumbnail" height="548" width="958">
+            </dt></dl>
+        </div>
+
+        <p>Toen we deze diagram aan het invullen waren, dachten we eraan om een campagne stunt te doen. hierdoor kunnen we veel mensen aanspreken. We dachten aan een mini event bij een hogeschool. Hierbij zal er een kleine tent op het gras worden gezet waarin je gratis drankjes krijgt en klassieke muziek hoort. Met dit event kunnen we dan een promovideo maken voor social media en op de website. Door dit allemaal uit te denken kwamen we uit op de customer journey. Hier staat welke media en hoe we dit gaan gebruiken bij de verschillende fases van de journey. Deze fases zijn awareness, consideration en engage. Met dit concept komen we tot engage, verder zullen we andere fases niet bereiken. We hebben elke media van het vorige diagram in de customer journey gezet. Bij sommige media hebben we uitgelegd op welke manier we die gaan gebruiken. Zo heb je bijvoorbeeld een social media share/like actie en videos vanaf youtube of de Facebook pagina. Dit hebben we op volgorde gezet zodat de route ernaar toe helemaal klopt.</p>
+       
+
+        <div id="gallery-3" class="gallery galleryid-2178 gallery-columns-3 gallery-size-thumbnail"><div class="clearboth"></div><dl class=" gallery-fullwidth">
+            <dt class="gallery-icon landscape">
+
+        <img src="../images/media1.jpg"class="attachment-thumbnail" height="548" width="958">
+            </dt></dl>
+        </div>
+
+        <p></p>
+
+
+
+
+        <div class="titelreflectie">
+            <span>
+            <h3>REFLECTIE</h3>
+            </span>
+        </div>
+
+        <p>Ik vond het heel nuttig om de mediastrategie uit te werken voor dit concept. Hierdoor kwamen we op meer media middelen waar we nog niet aan gedacht hadden. Met deze strategie weten we precies welke media we willen gaan gebruiken en hoe we dit gaan uitwerken. Ook de customer journey is een goede diagram voor het concept. Je kan duidelijk zien welke media wanneer worden gebruikt en in welke fase dit zit. Ik heb dit vorig jaar ook moeten maken, maar ik merk dat ik hier veel van geleerd heb. Die van vorig jaar was niet goed uitgewerkt en niet overzichtelijk. Met deze customer journey krijg je een duidelijk overzicht. </p><br>
+        </div>
+    </div>
+
+        <footer>
+                <div class="contacticon">
+        <div class="emailicondiv">
+        <a href="mailto:annemijn.portier@hotmail.com"><img id="emailicon" src="../images/emailicon.png"/></a>
+        </div>
+        <div class="linkedinicondiv">
+        <a href="https://www.linkedin.com/in/annemijn-portier-8b7914115?trk=nav_responsive_tab_profile"><img id="linkedinicon" src="../images/linkedinicon.png"/></a>
+      
+        </div>
+        </div>
+        <p>Copyright 2016 ACreations, All Rights Reserved.</p>
+        </footer>
+                        <script src="../js/modernizr.custom.js"></script>
+        		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script src="../js/jquery.dlmenu.js"></script>
+		<script>
+			$(function() {
+				$( '#dl-menu' ).dlmenu();
+			});
+		</script>
+<script src="../js/jquery.carouFredSel-6.0.2-packed.js" type="text/javascript"></script>
+<script src="../js/jquery.prettyPhoto.js" type="text/javascript"></script>
+<script src="../js/waypoints.min.js" type="text/javascript"></script>
+<script src="../js/custom.js" type="text/javascript"></script>
+
+</body>
+</html>
