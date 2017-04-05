9fcdbf10e4a043e87d8c48cf8a2b2dbcecda62c1
Unknown <annemijn.portier@hotmail.com>
2017-04-05 15:49:43 +0200

    added page week6 with content

---
+++ pages/week6.php
@@ -0,0 +1,153 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="../css/stylesheet.css" media="screen" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link href='http://fonts.googleapis.com/css?family=Raleway:200,300' rel='stylesheet' type='text/css'>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+  <link rel="apple-touch-icon" sizes="57x57" href="../images/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="../images/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="../images/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="../images/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="../images/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="../images/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="../images/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="../images/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="../images/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="../images/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="../images/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../images/favicon-16x16.png">
+<link rel="manifest" href="../js/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="../images/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
+<title>A Creations</title>
+    <script src="http://d3js.org/d3.v3.min.js" language="JavaScript"></script>
+    <script src="../js/liquidFillGauge.js" language="JavaScript"></script>
+</head>
+
+<body id="project">
+
+<header>
+<div id="dl-menu" class="dl-menuwrapper">
+						<button class="dl-trigger">Open Menu</button>
+						<ul class="dl-menu">
+							<li>
+								<a href="about.php">About me</a>
+								
+									</li>
+									<li>
+										<a href="portfolio.php">Portfolio</a>
+                                        </li>
+							<li>
+								 <li><a href="drawings.php">Drawings</a></li>	
+                                  <li><a href="contact.php">Contact</a></li>	
+								
+							</li>
+									
+								
+						</ul>
+                        </div>
+<nav>
+<div class="wrapper">
+
+<div class="logo"><a href="../index.php" ><img src="../images/logo.png" alt="ACreations"></a></div>
+
+</div>
+</nav>
+</header>
+
+
+
+    <div class="project-title-3d">
+            <h1>Conceptweek 6</h1>
+    </div>
+    <div class="wrapperproject">
+    <div class="single-post-arrows single-prev-post"><span><a style="left: -565.2px;" href="week4.php" class="single-prev-post-a"></a></span><a style="left: -565.2px; opacity: 0.2;" href="week4.php" rel="next">Conceptweek 5</a></div>
+    <div class="single-post-arrows single-next-post"><span><a style="right: -546.2px;" href="week6.php" class="single-next-post-a"></a></span><a style="right: -246.2px; opacity: 0.2;" href="week6.php" rel="prev">Conceptweek 6</a></div>
+    
+                     <div class="titelinleiding">
+            <span>
+            <h3>INLEIDING</h3>
+            </span>
+            </div>
+            
+    	<div class="projecttext">
+        <p>In deze week gaan we onze twee concepten laten zien aan de opdrachtgever in Arnhem. We gaan de presentatie voorbereiden en de concepten beter uitwerken.</p>
+        </div>
+                 <div class="titeluitleg">
+            <span>
+            <h3>UITLEG</h3>
+            </span>
+            </div>
+            
+    	<div class="projecttext">
+        <p>Maandag zijn we verder gegaan met de taken die we vorige week hadden verdeeld. We lieten aan elkaar zien wat we al hadden gemaakt. Ik liet de logo zien die ik had gemaakt en daarbij kreeg ik nog feedback puntjes. Dit heb ik toegepast en uiteindelijk is dit de logo geworden:</p>
+        
+
+        <div id="gallery-3" class="gallery galleryid-2178 gallery-columns-3 gallery-size-thumbnail"><div class="clearboth"></div><dl class=" gallery-fullwidth">
+            <dt class="gallery-icon landscape">
+
+        <img src="../images/logoconcept.png"class="attachment-thumbnail" height="548" width="958">
+            </dt></dl>
+        </div>
+
+        <p>Verder waren de dia’s voor de presentatie, de video’s en de posters ook af. Daarna zijn we gaan opschrijven wat we over de concepten wilden vertellen en is de presentatie gemaakt. Verder heb ik nog daarna mijn stuk die ik ging presenteren voorbereid. Ik heb wat steekwoorden opgeschreven die belangrijk waren om te vertellen. </p>
+
+        <p>Dinsdag was de conceptpresentatie bij Perplex. Ik ging presenteren met Arjan. Ik vond het presenteren spannend voor zoveel mensen. Ik vond dat het iets minder goed ging als vorige week. Dit kwam waarschijnlijk door de zenuwen. We kregen van Perplex een aantal feedback puntjes. Hij vond dat de visuals die we bij elke concept hadden gemaakt erg sterk. Wat hij wel jammer vond was dat we niet goed hadden onderbouwd waarom deze concepten jongeren zal aanspreken. We moesten wat meer onderzoek in de presentatie verwerken. We zijn daarna op een terrasje gaan zitten en gaan nadenken over welke concept we gaan uitwerken. We waren allemaal best snel eens om het concept Dolce far niente te kiezen en daarmee verder te gaan.
+        </p>
+
+        <p>Woensdag hebben we de feedback verwerkt van de presentatie. Daarna begonnen we met het scrumboard in te richten. Ik nam de taak op me. Het scrumboard had ik vorig jaar ook gedaan, dus ik wist precies hoe die eruit moet zien. Daarna zijn we de user stories gaan bedenken voor in de backlog. Ik merkte dat een aantal mensen niet zo goed snapte wat er moet de user stories wordt gedaan. Ik heb toen aan hun duidelijk uitgelegd hoe het precies in zijn werk gaat. Daarna vonden ze het een stuk duidelijker.  </p>
+
+        <div id="gallery-3" class="gallery galleryid-2178 gallery-columns-3 gallery-size-thumbnail"><div class="clearboth"></div><dl class=" gallery-fullwidth">
+            <dt class="gallery-icon landscape">
+
+        <img src="../images/week6.png"class="attachment-thumbnail" height="548" width="958">
+            </dt></dl>
+        </div>
+
+        <p>Nadat de user stories waren gemaakt, zijn we de taken gaan verzinnen voor deze sprint. De taken die we vandaag wilden doen waren een enquête maken en de mediastrategie uitwerken. We wilden graag een enquête rond sturen op Facebook voor onderzoek. Zo kunnen we in de volgende presentatie aantonen dat de jongeren dit concept aanspreken. We hebben ook de media strategie gemaakt. De resultaten kan je <a href="media.php">hier</a> zien. 
+        </p>
+
+
+        <div class="titelreflectie">
+            <span>
+            <h3>REFLECTIE</h3>
+            </span>
+        </div>
+
+        <p>Het was voor mij een leerzame week. De conceptpresentatie was erg nuttig en het voelde goed dat ik de taak op me nam om te presenteren. Het ging niet vlekkeloos, maar het ging ook niet slecht. Ik merk dat ik me er steeds zekerder bij voel. Ik laat de gedachte dat er zoveel mensen aanwezig zijn achter me en probeer ik zo duidelijk mogelijk mijn verhaal te vertellen. Door rustig te blijven en je tijd te nemen merk ik dat het bij mij goed gaat. Ik hoorde van andere groepsleden dat ze het goed vonden gaan, dus dat was ook een zelfverzekerdheid boost. Deze week nam ik de taak op me om de scrumboard in te richten en om aan iedereen uit te leggen hoe het werkt. Een aantal waren er al bekend mee, maar de meeste niet. Ik hoorde van ze dat ze het nu beter begrepen met mijn uitleg. Verder heb ik een duidelijke scrumboard gemaakt die we goed kunnen gebruiken bij elke sprint. </p><br>
+        </div>
+    </div>
+
+        <footer>
+                <div class="contacticon">
+        <div class="emailicondiv">
+        <a href="mailto:annemijn.portier@hotmail.com"><img id="emailicon" src="../images/emailicon.png"/></a>
+        </div>
+        <div class="linkedinicondiv">
+        <a href="https://www.linkedin.com/in/annemijn-portier-8b7914115?trk=nav_responsive_tab_profile"><img id="linkedinicon" src="../images/linkedinicon.png"/></a>
+      
+        </div>
+        </div>
+        <p>Copyright 2016 ACreations, All Rights Reserved.</p>
+        </footer>
+                        <script src="../js/modernizr.custom.js"></script>
+        		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script src="../js/jquery.dlmenu.js"></script>
+		<script>
+			$(function() {
+				$( '#dl-menu' ).dlmenu();
+			});
+		</script>
+<script src="../js/jquery.carouFredSel-6.0.2-packed.js" type="text/javascript"></script>
+<script src="../js/jquery.prettyPhoto.js" type="text/javascript"></script>
+<script src="../js/waypoints.min.js" type="text/javascript"></script>
+<script src="../js/custom.js" type="text/javascript"></script>
+
+</body>
+</html>
