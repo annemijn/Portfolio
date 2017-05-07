474181debfea40e0f29a796f8ffc8ca41b5a2af8
Unknown <annemijn.portier@hotmail.com>
2017-05-07 16:32:53 +0200

    added page app with content

---
+++ pages/app.php
@@ -0,0 +1,141 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="../css/stylesheet.css" media="screen" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link href='http://fonts.googleapis.com/css?family=Raleway:200,300' rel='stylesheet' type='text/css'>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+  <link rel="apple-touch-icon" sizes="57x57" href="../images/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="../images/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="../images/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="../images/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="../images/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="../images/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="../images/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="../images/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="../images/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="../images/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="../images/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../images/favicon-16x16.png">
+<link rel="manifest" href="../js/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="../images/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
+<title>A Creations</title>
+    <script src="http://d3js.org/d3.v3.min.js" language="JavaScript"></script>
+    <script src="../js/liquidFillGauge.js" language="JavaScript"></script>
+</head>
+
+<body id="project">
+
+<header>
+<div id="dl-menu" class="dl-menuwrapper">
+						<button class="dl-trigger">Open Menu</button>
+						<ul class="dl-menu">
+							<li>
+								<a href="about.php">About me</a>
+								
+									</li>
+									<li>
+										<a href="portfolio.php">Portfolio</a>
+                                        </li>
+							<li>
+								 <li><a href="drawings.php">Drawings</a></li>	
+                                  <li><a href="contact.php">Contact</a></li>	
+								
+							</li>
+									
+								
+						</ul>
+                        </div>
+<nav>
+<div class="wrapper">
+
+<div class="logo"><a href="../index.php" ><img src="../images/logo.png" alt="ACreations"></a></div>
+
+</div>
+</nav>
+</header>
+
+
+
+    <div class="project-title-natuur">
+            <h1>Get your app together</h1>
+    </div>
+    <div class="wrapperproject">
+    <div class="single-post-arrows single-prev-post"><span><a style="left: -565.2px;" href="hackathon.php" class="single-prev-post-a"></a></span><a style="left: -565.2px; opacity: 0.2;" href="hackathon.php" rel="next">Hackathon</a></div>
+    <div class="single-post-arrows single-next-post"><span><a style="right: -546.2px;" href="framework.php" class="single-next-post-a"></a></span><a style="right: -246.2px; opacity: 0.2;" href="framework.php" rel="prev">Zelfgeschreven framework</a></div>
+
+        <div class="titelreflectie">
+            <span>
+            <h3>REFLECTIE</h3>
+            </span>
+        </div>
+
+        <p>Met deze workshop heb ik geleerd om user stories  bij een prototype te bedenken en daarbij librarys te verzinnen. Ik vond dit een hele fijne manier. Zo kan je meteen te weten komen welke librarys erg handig zijn om te gebruiken. Dit kost minder tijd en werkt (meestal) meteen. Ook heb ik geleerd om een plugin van JQuery te gebruiken. Ik vond dit makkelijker gaan dan verwacht. In de documentatie was duidelijk aangegeven waar de belangrijkste punten kon aanpassen. Wel vond ik dat ik hierdoor niet meer echt de code zelf leerde begrijpen. Je gaat gemakkelijk de code die je niet wilt aanpassen overslaan. Hierdoor kan er ontstaan dat je errors niet kan verhelpen doordat je de code niet begrijpt.</p><br>
+
+                 <div class="titeluitleg">
+            <span>
+            <h3>UITLEG</h3>
+            </span>
+            </div>
+            
+    	<div class="projecttext">
+        <p>Voor het vak Dev gingen we met het groepje een prototype bedenken voor het concept. Wij hadden hier al over nagedacht en wilde graag een website maken. Dit gingen wij schetsen op het bord om de indeling van de website te laten zien. </p>
+
+        <div id="gallery-3" class="gallery galleryid-2178 gallery-columns-3 gallery-size-thumbnail"><div class="clearboth"></div><dl class=" gallery-fullwidth">
+            <dt class="gallery-icon landscape">
+
+        <img src="../images/app2.png"class="attachment-thumbnail" height="548" width="958">
+            </dt></dl>
+        </div>
+        
+        <p>Daarna moesten we een aantal user stories bedenken bij de prototype. Hierbij ging het om wat de gebruiker graag wilt op de website. We hadden veel user stories bedacht. Hierbij wat voorbeelden:</p>
+
+        <p>-    Als gebruik wil ik een datum zien zodat ik weet wanneer het festival is. <br>
+            -   Als gebruiker wil ik de prijs van de tickets zien zodat ik weet hoeveel geld ik moet uitgeven.<br>
+            -   Als gebruiker wil ik het programma hebben van het evenement zodat ik weet wat er te doen is.<br>
+            -   Als gebruiker wil ik een beeld krijgen van het festival zodat ik overgehaald kan worden om deze te bezoeken.<br>
+            -   Als gebruiker wil ik een overzicht te zien krijgen van de verschillende tickets zodat ik een geschikte ticket kan kopen.</p>
+        
+        <p>Nadat we de userstories hadden verzonnen, moesten we een library bij een user story koppelen. Door deze librarys te verzamelen kan je straks gemakkelijker de website maken zonder teveel zelf te programmeren. Ik pakte de user story: Als gebruik wil ik een datum zien zodat ik weet wanneer het festival is. Hierbij was ik op zoek naar een count down. Dit laat zien hoelang het nog duurt voordat het begint. Ik kwam al gauw op de library van jQuery die een plugin had genaamd “the final countdown”. Dit is een duidelijke count down waarmee je snel eentje kan neerzetten. Dit scheelt veel programmeerwerk. Op de website staat er duidelijke documentatie om met de plugin te werken. Dit maakt het makkelijker om code aan te passen. Ik heb de plugin geïnstalleerd en zelf een count down gemaakt. </p>
+
+
+
+        <p>Wat me opviel is dat ik best makkelijk de count down werkend kon maken. Verder merkte ik wel dat ik de code niet altijd goed begreep. In de documentatie staat het lastig uitgelegd en snapte ik een aantal termen niet. Als je al een aantal termen niet begrijp, weet je niet hoe de code in elkaar steekt. Daarom dat ik het ook best lastig vond om het uitgebreid aan te passen. Wat wel makkelijk was is de tijd aan te passen. Je moet de datum intikken waarop het evenement plaats vind. Hiervan berekend die hoeveel tijd er nog over is. Deze kan je aangeven in dagen, uren, minuten en seconde. Dit is ook makkelijk aan te passen. </p>
+        
+
+        </div>
+    </div>
+
+        <footer>
+                <div class="contacticon">
+        <div class="emailicondiv">
+        <a href="mailto:annemijn.portier@hotmail.com"><img id="emailicon" src="../images/emailicon.png"/></a>
+        </div>
+        <div class="linkedinicondiv">
+        <a href="https://www.linkedin.com/in/annemijn-portier-8b7914115?trk=nav_responsive_tab_profile"><img id="linkedinicon" src="../images/linkedinicon.png"/></a>
+      
+        </div>
+        </div>
+        <p>Copyright 2016 ACreations, All Rights Reserved.</p>
+        </footer>
+                        <script src="../js/modernizr.custom.js"></script>
+        		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script src="../js/jquery.dlmenu.js"></script>
+		<script>
+			$(function() {
+				$( '#dl-menu' ).dlmenu();
+			});
+		</script>
+<script src="../js/jquery.carouFredSel-6.0.2-packed.js" type="text/javascript"></script>
+<script src="../js/jquery.prettyPhoto.js" type="text/javascript"></script>
+<script src="../js/waypoints.min.js" type="text/javascript"></script>
+<script src="../js/custom.js" type="text/javascript"></script>
+
+</body>
+</html>
