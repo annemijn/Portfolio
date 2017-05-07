8ed0c1c5751a9d0f7aa4ff3aa3c8ee276d4df641
Unknown <annemijn.portier@hotmail.com>
2017-05-07 19:21:44 +0200

    added page scrumsprint1 with content

---
+++ pages/scrumsprint1.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="../css/stylesheet.css" media="screen" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link href='http://fonts.googleapis.com/css?family=Raleway:200,300' rel='stylesheet' type='text/css'>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+  <link rel="apple-touch-icon" sizes="57x57" href="../images/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="../images/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="../images/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="../images/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="../images/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="../images/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="../images/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="../images/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="../images/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="../images/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="../images/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../images/favicon-16x16.png">
+<link rel="manifest" href="../js/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="../images/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
+<title>A Creations</title>
+    <script src="http://d3js.org/d3.v3.min.js" language="JavaScript"></script>
+    <script src="../js/liquidFillGauge.js" language="JavaScript"></script>
+        <script src="../js/jquery.countdown.js" language="JavaScript"></script>
+</head>
+
+<body id="project">
+
+<header>
+<div id="dl-menu" class="dl-menuwrapper">
+                        <button class="dl-trigger">Open Menu</button>
+                        <ul class="dl-menu">
+                            <li>
+                                <a href="about.php">About me</a>
+                                
+                                    </li>
+                                    <li>
+                                        <a href="portfolio.php">Portfolio</a>
+                                        </li>
+                            <li>
+                                 <li><a href="drawings.php">Drawings</a></li>   
+                                  <li><a href="contact.php">Contact</a></li>    
+                                
+                            </li>
+                                    
+                                
+                        </ul>
+                        </div>
+<nav>
+<div class="wrapper">
+
+<div class="logo"><a href="../index.php" ><img src="../images/logo.png" alt="ACreations"></a></div>
+
+</div>
+</nav>
+</header>
+
+
+
+    <div class="project-title-fme">
+            <h1>SCRUM sprint 1</h1>
+    </div>
+    <div class="wrapperproject">
+    <div style="display: none;" class="single-post-arrows single-prev-post"><span><a class="single-prev-post-a"></a></span></div>
+    <div class="single-post-arrows single-next-post"><span><a style="right: -546.2px;" href="scrumsprint2.php" class="single-next-post-a"></a></span><a style="right: -246.2px; opacity: 0.2;" href="scrumsprint2.php" rel="prev">SCRUM sprint 2</a></div>
+
+        <div class="titelreflectie">
+            <span>
+            <h3>REFLECTIE</h3>
+            </span>
+        </div>
+
+        <p>ik merkte dat het scrummen in ons groepje moeilijk op gang ging. Sommige groepsleden begonnen met een taak zonder dat dit op het scrumboard stond. Ik heb ze aangesproken om dit eerst met het groepje te bespreken. Verder vond ik het leerzaam om scrum master te zijn. Je moet de scrumboard goed bijhouden, zodat het klopt met de planning. Ook moet je vragen aan elke groepslid of ze hun taak hebben gedaan en wat voor taak ze gaan doen. Het is een verantwoordelijkheid die je bij moet houden. </p><br>
+
+                 <div class="titeluitleg">
+            <span>
+            <h3>UITLEG</h3>
+            </span>
+            </div>
+            
+        <div class="projecttext">
+        <p>In sprint 1 zijn we begonnen met SCRUM. Daarbij zijn we de scrumboard gaan inrichten. Omdat ik dit semester al een keer had gedaan, wist ik precies hoe dit eruit moest zien. ik had de scrumboard getekend. We hebben een productbacklog. Hierin staan alle userstories die aan het einde van het semester voltooid moeten zijn. Daarnaast staat de sprintbacklog. Hierin staan de userstories die deze sprint af moeten zijn. Uit deze userstories komen taken. Deze taken staan in de to do. De taken worden dan verdeeld over de groepsleden, dit staat in de in progress gedeelte. Dit is opgedeeld voor elke groepslid. Als de taken zijn voltooid komen die bij done te staan. </p>
+
+        <div id="gallery-3" class="gallery galleryid-2178 gallery-columns-3 gallery-size-thumbnail"><div class="clearboth"></div><dl class=" gallery-fullwidth">
+            <dt class="gallery-icon landscape">
+
+        <img src="../images/week6.png"class="attachment-thumbnail" height="548" width="958">
+            </dt></dl>
+        </div>
+
+        <p>Ik nam als taak om de scrum master te zijn. Zo schreef ik de post its en vroeg ik bij de daily stand ups hoe het met iedereen zijn taken ging. Ik heb aan drie van mijn groepsleden ook uitgelegd hoe de scrumboard werkt. Zij hadden namelijk nog nooit met scrum gewerkt. Ze snappen nu wat een backlog inhoudt en ze vonden het fijn dat ik het had uitgelegd, want het was nu logisch voor ze.</p>
+
+        </div>
+    </div>
+
+        <footer>
+                <div class="contacticon">
+        <div class="emailicondiv">
+        <a href="mailto:annemijn.portier@hotmail.com"><img id="emailicon" src="../images/emailicon.png"/></a>
+        </div>
+        <div class="linkedinicondiv">
+        <a href="https://www.linkedin.com/in/annemijn-portier-8b7914115?trk=nav_responsive_tab_profile"><img id="linkedinicon" src="../images/linkedinicon.png"/></a>
+      
+        </div>
+        </div>
+        <p>Copyright 2016 ACreations, All Rights Reserved.</p>
+        </footer>
+                        <script src="../js/modernizr.custom.js"></script>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script src="../js/jquery.dlmenu.js"></script>
+        <script>
+            $(function() {
+                $( '#dl-menu' ).dlmenu();
+            });
+        </script>
+<script src="../js/jquery.carouFredSel-6.0.2-packed.js" type="text/javascript"></script>
+<script src="../js/jquery.prettyPhoto.js" type="text/javascript"></script>
+<script src="../js/waypoints.min.js" type="text/javascript"></script>
+<script src="../js/custom.js" type="text/javascript"></script>
+
+</body>
+</html>
