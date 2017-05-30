c4b6733962f7107091bf9b427e899e16fd298aa9
Unknown <annemijn.portier@hotmail.com>
2017-05-30 14:10:55 +0200

    added page test with content

---
+++ pages/test.php
@@ -0,0 +1,125 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="../css/stylesheet.css" media="screen" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link href='http://fonts.googleapis.com/css?family=Raleway:200,300' rel='stylesheet' type='text/css'>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+  <link rel="apple-touch-icon" sizes="57x57" href="../images/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="../images/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="../images/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="../images/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="../images/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="../images/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="../images/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="../images/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="../images/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="../images/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="../images/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../images/favicon-16x16.png">
+<link rel="manifest" href="../js/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="../images/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
+<title>A Creations</title>
+    <script src="http://d3js.org/d3.v3.min.js" language="JavaScript"></script>
+    <script src="../js/liquidFillGauge.js" language="JavaScript"></script>
+</head>
+
+<body id="project">
+
+<header>
+<div id="dl-menu" class="dl-menuwrapper">
+						<button class="dl-trigger">Open Menu</button>
+						<ul class="dl-menu">
+							<li>
+								<a href="about.php">About me</a>
+								
+									</li>
+									<li>
+										<a href="portfolio.php">Portfolio</a>
+                                        </li>
+							<li>
+								 <li><a href="drawings.php">Drawings</a></li>	
+                                  <li><a href="contact.php">Contact</a></li>	
+								
+							</li>
+									
+								
+						</ul>
+                        </div>
+<nav>
+<div class="wrapper">
+
+<div class="logo"><a href="../index.php" ><img src="../images/logo.png" alt="ACreations"></a></div>
+
+</div>
+</nav>
+</header>
+
+
+
+    <div class="project-title-concepttwee">
+            <h1>Usabillity testing</h1>
+    </div>
+    <div class="wrapperproject">
+    <div class="single-post-arrows single-prev-post"><span><a style="left: -565.2px;" href="app.php" class="single-prev-post-a"></a></span><a style="left: -565.2px; opacity: 0.2;" href="app.php" rel="next">Prototype: Campagne stunt</a></div>
+    <div class="single-post-arrows single-next-post"><span><a style="right: -546.2px;" href="app.php" class="single-next-post-a"></a></span><a style="right: -246.2px; opacity: 0.2;" href="app.php" rel="prev">Prototype: App</a></div>
+
+        <div class="titelreflectie">
+            <span>
+            <h3>REFLECTIE</h3>
+            </span>
+        </div>
+
+        <p>Ik vond het leerzaam om een usabillity test uit te voeren voor onze website. Wat ik heel fijn vind aan usabillity testing is dat er eerst een testplan wordt opgesteld. Hiermee kan je goed de test uitvoeren. Verder vond ik het ook fijn dat er iemand observeert en iemand notuleert. Hierdoor heb je meerdere mensen die de test analyseren en kan je meer informatie uiteindelijk krijgen. Ik vond het ook fijn om de verschillende rollen te zijn. Als je degene bent die de vragen stelt, merk je dat je vooral bezig bent met het luisteren naar de persoon, terwijl als je notuleert je ook meer kijkt wat voor handelingen hij doet. Daarom zijn deze twee rollen erg belangrijk om te hebben bij een usabillity test. Ik vond het ook fijn om vragen te stellen waardoor ik me meer in het interviewen kon verbeteren. Ik liet de testpersoon zijn gang gaan bij de scenario’s en liet vooral niet mijn mening horen. Ook bleef ik even stil zodat hij kon nadenken over de antwoord die hij wilde geven. Verder waren de resultaten van de test heel nuttig voor onze website. Hierdoor kunnen we het weer gaan verbeteren.
+        </p><br>
+
+                 <div class="titeluitleg">
+            <span>
+            <h3>UITLEG</h3>
+            </span>
+            </div>
+            
+    	<div class="projecttext">
+        <p>Voor de usabillity test wilde we onze website gaan testen. Hiervoor moest er eerst een testplan voor worden gemaakt. Daarvoor heb ik de moderating techniek uitgeschreven. We wilde gebruik maken van Think Aloud. voordat we de test starten vragen we aan de testpersonen of ze hardop willen denken, zodat we weten welke handelingen ze waarom doen. Dit helpt ons om een beeld te krijgen van de denk / handelswijze van onze testpersonen. Daarna heb ik de inleiding geschreven. Hierin heb ik onze doel uitgelegd van de usabillity test. Daarna zijn we met de groep de vragen en scenario’s gaan bedenken. Elke scenario bestond uit een vraag, taak en scenario. Daarnaast hadden we nog extra vragen die we op het laatst wilde stellen. De testplan kan je <a href="../Testplan.pdf">hier</a> lezen </p>
+
+        <p>Bij de eerste testpersoon had ik als rol die de vragen stelde. Bij elke scenario/vraag gaf ik hem voldoende tijd om het uit te voeren, zodat we genoeg informatie kregen en niet door mij de resultaat werd beïnvloed. Hierbij merkte ik dat ik mezelf vooral in de achtergrond moest treden en hem zelf alles laten doen. bij twee andere testpersonen heb ik ook genotuleerd. Hierbij merkte ik dat ik vooral de handelingen die de testpersoon goed moest bijhouden. Dit was namelijk heel belangrijk om op te schrijven.  </p>
+
+        <p>Ik vond dat deze usabillity test heel nuttig is bij het verbeteren van de website. Waarbij er vooral de gebruiksvriendelijkheid verbeterd kan worden. Zo was bijvoorbeeld de locatie op google maps niet handig te lezen en wilde de meeste testpersonen meer informatie over deze locatie hebben. Bijvoorbeeld een routebeschrijving. </p>
+        
+
+        </div>
+    </div>
+
+        <footer>
+                <div class="contacticon">
+        <div class="emailicondiv">
+        <a href="mailto:annemijn.portier@hotmail.com"><img id="emailicon" src="../images/emailicon.png"/></a>
+        </div>
+        <div class="linkedinicondiv">
+        <a href="https://www.linkedin.com/in/annemijn-portier-8b7914115?trk=nav_responsive_tab_profile"><img id="linkedinicon" src="../images/linkedinicon.png"/></a>
+      
+        </div>
+        </div>
+        <p>Copyright 2016 ACreations, All Rights Reserved.</p>
+        </footer>
+                        <script src="../js/modernizr.custom.js"></script>
+        		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script src="../js/jquery.dlmenu.js"></script>
+		<script>
+			$(function() {
+				$( '#dl-menu' ).dlmenu();
+			});
+		</script>
+<script src="../js/jquery.carouFredSel-6.0.2-packed.js" type="text/javascript"></script>
+<script src="../js/jquery.prettyPhoto.js" type="text/javascript"></script>
+<script src="../js/waypoints.min.js" type="text/javascript"></script>
+<script src="../js/custom.js" type="text/javascript"></script>
+
+</body>
+</html>
