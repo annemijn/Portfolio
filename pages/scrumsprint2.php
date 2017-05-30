af079ae21d37e7e240d7f8e5289b38e6527eb8e0
Unknown <annemijn.portier@hotmail.com>
2017-05-30 14:35:32 +0200

    added page scrumsprint 2 with content

---
+++ pages/scrumsprint2.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="../css/stylesheet.css" media="screen" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link href='http://fonts.googleapis.com/css?family=Raleway:200,300' rel='stylesheet' type='text/css'>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+  <link rel="apple-touch-icon" sizes="57x57" href="../images/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="../images/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="../images/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="../images/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="../images/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="../images/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="../images/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="../images/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="../images/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="../images/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="../images/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../images/favicon-16x16.png">
+<link rel="manifest" href="../js/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="../images/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
+<title>A Creations</title>
+    <script src="http://d3js.org/d3.v3.min.js" language="JavaScript"></script>
+    <script src="../js/liquidFillGauge.js" language="JavaScript"></script>
+        <script src="../js/jquery.countdown.js" language="JavaScript"></script>
+</head>
+
+<body id="project">
+
+<header>
+<div id="dl-menu" class="dl-menuwrapper">
+                        <button class="dl-trigger">Open Menu</button>
+                        <ul class="dl-menu">
+                            <li>
+                                <a href="about.php">About me</a>
+                                
+                                    </li>
+                                    <li>
+                                        <a href="portfolio.php">Portfolio</a>
+                                        </li>
+                            <li>
+                                 <li><a href="drawings.php">Drawings</a></li>   
+                                  <li><a href="contact.php">Contact</a></li>    
+                                
+                            </li>
+                                    
+                                
+                        </ul>
+                        </div>
+<nav>
+<div class="wrapper">
+
+<div class="logo"><a href="../index.php" ><img src="../images/logo.png" alt="ACreations"></a></div>
+
+</div>
+</nav>
+</header>
+
+
+
+    <div class="project-title-conceptdrie">
+            <h1>SCRUM sprint 2</h1>
+    </div>
+    <div class="wrapperproject">
+    <div class="single-post-arrows single-prev-post"><span><a style="left: -565.2px;" href="scrumsprint1.php" class="single-prev-post-a"></a></span><a style="left: -565.2px; opacity: 0.2;" href="scrumsprint1.php" rel="next">SCRUM sprint 1</a></div>
+    <div class="single-post-arrows single-next-post"><span><a style="right: -546.2px;" href="scrumsprint2.php" class="single-next-post-a"></a></span><a style="right: -246.2px; opacity: 0.2;" href="scrumsprint2.php" rel="prev">SCRUM sprint 3</a></div>
+
+        <div class="titelreflectie">
+            <span>
+            <h3>REFLECTIE</h3>
+            </span>
+        </div>
+
+        <p>Het scrummen in deze sprint ging een stuk beter dan in sprint 1. Ik merkte dat mijn groepsleden steeds meer begrepen wat scrum inhield. Elke dag heb ik de scrumboard geupdate als scrummaster. Ook heb ik elke dag gevraagd aan de groepsleden of ze hun taak af hadden. Daarna ben ik de taken gaan verdelen voor die dag. Ik ging steeds meer in de scrum master rol zitten en ik raakte eraan gewend. Dit merkte ik ook bij mijn groepsleden. Verder vond ik het fijn om de retrospective te doen. Hiermee kwam je achter wat er de vorige sprint goed ging en wat beter kon. Dit was ook heel fijn om van de andere groepsleden te weten, zodat je erover kon praten. Aan de verbeterpunten kunnen we in de volgende sprint aan werken.</p><br>
+
+                 <div class="titeluitleg">
+            <span>
+            <h3>UITLEG</h3>
+            </span>
+            </div>
+            
+        <div class="projecttext">
+        <p>Na de sprintdemo zijn we een retrospective gaan doen. Daarbij gingen we na wat er in sprint 1 goed ging en minder goed ging. Ik vond dat we niet snel beslissingen maakte. We bleven te lang bij een onderwerp zitten en daarna hadden we nog steeds niks. Ik vond dat we sneller de knoop moesten doorhakken. Iedereen was het hiermee eens. Verder vond ik dat we elke dag productief bezig waren. Iedereen werkte goed en had de taak voor de deadline af. Ook is iedereen elke dag op tijd aanwezig. Dit helpt ook mee aan de productiviteit. </p>
+
+        <div id="gallery-3" class="gallery galleryid-2178 gallery-columns-3 gallery-size-thumbnail"><div class="clearboth"></div><dl class=" gallery-fullwidth">
+            <dt class="gallery-icon landscape">
+
+        <img src="../images/retrospective.png"class="attachment-thumbnail" height="548" width="958">
+            </dt></dl>
+        </div>
+
+        <p>Daarna ben ik de scrumboard gaan updaten. We zijn nagegaan welke user stories we afhadden. Het bleek dat er geen een nog voltooid kon worden van sprint 1. We hebben dus alle user stories laten staan en nog een aantal nieuwe verzonnen. </p>
+
+        <div id="gallery-3" class="gallery galleryid-2178 gallery-columns-3 gallery-size-thumbnail"><div class="clearboth"></div><dl class=" gallery-fullwidth">
+            <dt class="gallery-icon landscape">
+
+        <img src="../images/scrumsprint2.png"class="attachment-thumbnail" height="548" width="958">
+            </dt></dl>
+        </div>
+
+        <p>Ik heb in sprint 2 elke dag aan de groepsleden gevraagd hoe het met hun taken ging en welke ze vandaag gaan doen. doordat iedereen goed meewerkte bleef de scrumboard up to date. Het ging stuk makkelijker dan bij sprint 1. Iedereen snapt nu hoe scrummen werkt. Ikzelf vond het als scrum master nu ook veel fijner werken. de scrumboard klopte en zo weet je ook waar iedereen mee bezig is.
+        </p>
+
+
+        </div>
+    </div>
+
+        <footer>
+                <div class="contacticon">
+        <div class="emailicondiv">
+        <a href="mailto:annemijn.portier@hotmail.com"><img id="emailicon" src="../images/emailicon.png"/></a>
+        </div>
+        <div class="linkedinicondiv">
+        <a href="https://www.linkedin.com/in/annemijn-portier-8b7914115?trk=nav_responsive_tab_profile"><img id="linkedinicon" src="../images/linkedinicon.png"/></a>
+      
+        </div>
+        </div>
+        <p>Copyright 2016 ACreations, All Rights Reserved.</p>
+        </footer>
+                        <script src="../js/modernizr.custom.js"></script>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script src="../js/jquery.dlmenu.js"></script>
+        <script>
+            $(function() {
+                $( '#dl-menu' ).dlmenu();
+            });
+        </script>
+<script src="../js/jquery.carouFredSel-6.0.2-packed.js" type="text/javascript"></script>
+<script src="../js/jquery.prettyPhoto.js" type="text/javascript"></script>
+<script src="../js/waypoints.min.js" type="text/javascript"></script>
+<script src="../js/custom.js" type="text/javascript"></script>
+
+</body>
+</html>
